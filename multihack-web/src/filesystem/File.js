e40068423cec1ac1d935065963b058ed5471dd78
Egor Rumyantsev <RumyantsevGT@ya.ru>
2018-11-13 14:37:14 +0300

    upload

---
+++ multihack-web/src/filesystem/File.js
@@ -0,0 +1,43 @@
+/* globals CodeMirror */
+
+var util = require('./util')
+
+function File (path) {
+  var self = this
+  if (!(self instanceof File)) return new File()
+
+  self.name = util.getFilename(path)
+  self.path = path
+  self.isDir = false
+  self.viewMapping = util.getViewMapping(path)
+  self.alreadyLink = false
+
+  self.doc = new CodeMirror.Doc('', util.pathToMode(path))
+
+  Object.defineProperty(self, 'content', {
+    get: function () {
+      return self.doc.getValue()
+    }
+  })
+
+  Object.defineProperty(self, 'size', {
+    get: function () {
+      return self.doc.getValue().length
+    }
+  })
+}
+
+File.prototype.write = function (content, cb) {
+  var self = this
+
+  self.doc.setValue(content)
+  if (cb) cb()
+}
+
+File.prototype.read = function (cb) {
+  var self = this
+
+  if (cb) cb(self.doc.getValue())
+}
+
+module.exports = File
